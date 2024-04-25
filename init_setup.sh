72eb9b1c52f20ddc4dad47b2b6d7a62863223d6d
JuneChul Roh <jroh@ti.com>
2024-04-25 16:10:39 -0500

    Updated init_setup.sh: save SDK tag info into a file. also other clean up.

--- init_setup.sh
+++ init_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 SCRIPT_DIR=$PWD
 
 : "${SOC:=j721e}"
@@ -8,11 +9,12 @@ WORKAREA=$SCRIPT_DIR/${SOC}-workarea
 # pull the source repos
 # https://git.ti.com/cgit/processor-sdk/psdk_repo_manifests/refs/?h=main
 REPO_TAG=REL.PSDK.ANALYTICS.09.00.01.01
-# PDK_TAG=REL.PSDK.09.00.00.45
+# REPO_TAG=REL.PSDK.ANALYTICS.09.02.00.05
 
 # targetfs and rootfs info
 # http://edgeaisrv2.dhcp.ti.com/publish/prod/PROCESSOR-SDK-LINUX-${DEVICE_NAME}/
 PSDK_LINUX_VERSION=09_00_01_03
+# PSDK_LINUX_VERSION=09_02_00_05
 
 DEVICE_PLATFORM=${SOC}
 if [ "${SOC}" == "j721e" ]; then
@@ -32,32 +34,29 @@ PSDK_LINUX_ROOTFS=tisdk-${TISDK_IMAGE}-image-${DEVICE_PLATFORM}-evm.tar.xz
 PSDK_LINUX_BOOTFS=boot-${TISDK_IMAGE}-${DEVICE_PLATFORM}-evm.tar.gz
 PSDK_LINUX_WEBLINK=http://edgeaisrv2.dhcp.ti.com/publish/prod/PROCESSOR-SDK-LINUX-${DEVICE_NAME}/${PSDK_LINUX_VERSION}/exports
 
-if [ ! -d $WORKAREA ]; then
-    mkdir -p $WORKAREA
-    cd $WORKAREA
-    repo init -u git://git.ti.com/processor-sdk/psdk_repo_manifests.git -b refs/tags/${REPO_TAG} -m vision_apps_yocto.xml
-    repo sync
-
-    # pull PDK repo: only required to set env variables
-    # git clone --single-branch --branch ${PDK_TAG} git://git.ti.com/processor-sdk/pdk.git pdk
-
-    # apply fixes/workarounds. Modular scripts from setup_psdk_rtos.sh => TODO
-    cd $SCRIPT_DIR
-    cp patches/sdk_builder/concerto/compilers/gcc_linux_arm.mak ${WORKAREA}/sdk_builder/concerto/compilers/gcc_linux_arm.mak
-    Files=(
-        setup_psdk_rtos.sh
-        setup_tools_apt.sh
-        setup_tools_arm.sh
-        setup_tools_cgt.sh
-        setup_tools_misc.sh
-    )
-    for File in ${Files[@]}; do
-	    cp patches/sdk_builder/scripts/${File} ${WORKAREA}/sdk_builder/scripts/${File}
+# define a function to save selected environment variables
+save_env_vars() {
+    output_file="sdk_variables.txt"
+    env_vars=("REPO_TAG" "PSDK_LINUX_VERSION" "SOC"
+        "DEVICE_NAME" "DEVICE_PLATFORM" "PSDK_LINUX_ROOTFS"
+        "PSDK_LINUX_BOOTFS" "PSDK_LINUX_WEBLINK")
+    for var in "${env_vars[@]}"; do
+        echo "$var=${!var}" >> $output_file
     done
-    # remove the 'u' flag for AR to avoid warning messages in aarch64 Ubuntu container
-    cp patches/sdk_builder/concerto/compilers/gcc_linux_arm.mak ${WORKAREA}/sdk_builder/concerto/compilers/gcc_linux_arm.mak
+    echo "Selected environment variables have been saved to $output_file"
+}
 
-    # download and install PSDK Linux target FS and boot image
+# define a function to copy a file and backup the original if it exists
+copy_and_backup() {
+    src_file=$1
+    dest_file=$2
+    if [ -f "$dest_file" ]; then
+        mv  $dest_file $dest_file.ORG
+    fi
+    cp $src_file $dest_file
+}
+
+download_targetfs() {
     cd $WORKAREA
     echo "[psdk linux ${PSDK_LINUX_ROOTFS}] Checking ..."
     if [ ! -d targetfs ]; then
@@ -70,7 +69,10 @@ if [ ! -d $WORKAREA ]; then
         fi
     fi
     echo "[psdk linux ${PSDK_LINUX_ROOTFS}] Done "
+}
 
+download_rootfs() {
+    cd $WORKAREA
     echo "[psdk linux ${PSDK_LINUX_BOOTFS}] Checking ... "
     if [ ! -d bootfs ]; then
         curl -O ${PSDK_LINUX_WEBLINK}/${PSDK_LINUX_BOOTFS}
@@ -82,6 +84,38 @@ if [ ! -d $WORKAREA ]; then
         fi
     fi
     echo "[psdk linux ${PSDK_LINUX_BOOTFS}] Done "
+}
+
+if [ ! -d $WORKAREA ]; then
+    mkdir -p $WORKAREA
+    cd $WORKAREA
+
+    # save selected environment variables
+    save_env_vars
+
+    # repo sync
+    repo init -u git://git.ti.com/processor-sdk/psdk_repo_manifests.git -b refs/tags/${REPO_TAG} -m vision_apps_yocto.xml
+    repo sync
+
+    # apply fixes/workarounds. Modular scripts from setup_psdk_rtos.sh => TODO
+    # Modular scripts prepared for SDK 9.0
+    cd $SCRIPT_DIR
+    Files=(
+        setup_psdk_rtos.sh
+        setup_tools_apt.sh
+        setup_tools_arm.sh # only this is used in this build system
+        setup_tools_cgt.sh
+        setup_tools_misc.sh
+    )
+    for File in ${Files[@]}; do
+        copy_and_backup patches/sdk_builder/scripts/${File} ${WORKAREA}/sdk_builder/scripts/${File}
+    done
+    # remove the 'u' flag for AR to avoid warning messages in aarch64 Ubuntu container
+    copy_and_backup patches/sdk_builder/concerto/compilers/gcc_linux_arm.mak ${WORKAREA}/sdk_builder/concerto/compilers/gcc_linux_arm.mak
+
+    # download and install PSDK Linux target FS and boot image
+    download_targetfs
+    download_rootfs
 
 else
     echo "$WORKAREA already exists."
